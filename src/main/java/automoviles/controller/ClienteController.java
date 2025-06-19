cfc0a92ad1a8add8255f416bf89a02b9e73d4304
roosivelt-td <roosivelt.td@gmail.com>
2025-06-18 22:36:39 -0500

    cliente 0.0.1

---
+++ src/main/java/automoviles/controller/ClienteController.java
@@ -0,0 +1,47 @@
+package automoviles.controller;
+
+import automoviles.dto.request.ClienteRequest;
+import automoviles.dto.response.ClienteResponse;
+import automoviles.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/api/clientes")
+public class ClienteController {
+    @Autowired
+    private ClienteService clienteService;
+
+    @PostMapping ("/create")// crear un cliente
+    public void crearCliente(@RequestBody ClienteRequest request) { clienteService.crearCliente(request);
+    }
+
+    @GetMapping("/{id}") // obtener un cliente por su id
+    public ResponseEntity<ClienteResponse> obtenerClientePorId(@PathVariable Long id) {
+        return ResponseEntity.ok(clienteService.obtenerClientePorId(id));
+    }
+
+    @GetMapping("/todos")// obtener todos los clientes
+    public ResponseEntity<Collection<ClienteResponse>> obtenerTodosLosClientes() {
+        return ResponseEntity.ok(clienteService.obtenerTodosLosClientes());
+    }
+
+    @PutMapping("/update/{id}")// actualizar un cliente por id
+    public void actualizarClienteId(@PathVariable Long id, @RequestBody ClienteRequest request) {
+        if (clienteService.obtenerClientePorId(id) != null) {
+            clienteService.actualizarCliente(id, request);
+        } else {
+            throw new RuntimeException("No existe un cliente con el id: " + id);
+        }
+    }
+
+    @DeleteMapping("/delete/{id}") // eliminar un cliente  por id
+    public void eliminarClienteId(@PathVariable Long id) {
+        clienteService.eliminarCliente(id);
+    }
+
+
+}
