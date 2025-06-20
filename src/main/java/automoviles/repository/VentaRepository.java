2324c08ce186289db4f28a19cc4ba4b3ae50b719
Brancho825 <sanchezjhim502@gmail.com>
2025-06-19 23:51:06 -0500

    Este código define la interfaz VentaRepository, que extiende JpaRepository para gestionar ventas en la base de datos. Gracias a esto, Spring genera automáticamente métodos como guardar, buscar y eliminar ventas sin escribir SQL manual.

--- src/main/java/automoviles/repository/VentaRepository.java
+++ src/main/java/automoviles/repository/VentaRepository.java
@@ -1,1 +1,12 @@
 package automoviles.repository;
+
+import automoviles.model.Venta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface VentaRepository extends JpaRepository<Venta, Long> {
+
+}
