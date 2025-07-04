d9ef18a18c06fff3e988c702b7c3488d4f3f4355
leocrispaitan <leocrispaitan@gmail.com>
2025-07-04 14:19:28 -0500

    Update UsuarioController.java

--- src/main/java/automoviles/controller/UsuarioController.java
+++ src/main/java/automoviles/controller/UsuarioController.java
@@ -1,6 +1,7 @@
 package automoviles.controller;
 
 import automoviles.auth.entity.User;
+import automoviles.auth.repository.UserRepository;
 import automoviles.dto.request.UsuarioRequest;
 import automoviles.dto.response.UsuarioResponse;
 import automoviles.service.UsuarioService;
@@ -10,6 +11,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/usuarios")
@@ -17,9 +19,13 @@ public class UsuarioController {
 
     @Autowired
     private UsuarioService usuarioService;
+    
+    @Autowired
+    private UserRepository userRepository;
 
     @PostMapping ("/create")// crear un usuario
-    public void crearUsuario(@RequestBody UsuarioRequest request) { usuarioService.crearUsuario(request);
+    public ResponseEntity<UsuarioResponse> crearUsuario(@RequestBody UsuarioRequest request) { 
+        return ResponseEntity.ok(usuarioService.crearUsuario(request));
     }
 
     @GetMapping("/{id}") // obtener un usuario por su id
@@ -30,6 +36,11 @@ public class UsuarioController {
     @GetMapping("/todos")// obtener todos los usuarios
     public ResponseEntity<Collection<UsuarioResponse>> obtenerTodosLosUsuarios() {
        return ResponseEntity.ok(usuarioService.obtenerTodosLosUsuarios());
+    }
+    
+    @GetMapping("/users-disponibles")// obtener todos los Users disponibles
+    public ResponseEntity<List<User>> obtenerUsersDisponibles() {
+       return ResponseEntity.ok(userRepository.findAll());
     }
 
     @PutMapping("/update/{id}")// actualizar un usuario por id
