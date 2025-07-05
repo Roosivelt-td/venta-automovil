f639c1d27ff1e3a442cd7762ea3dbcbf8cfb59e2
leocrispaitan <leocrispaitan@gmail.com>
2025-07-04 21:35:15 -0500

    Update AutoResponse.java

--- src/main/java/automoviles/dto/response/AutoResponse.java
+++ src/main/java/automoviles/dto/response/AutoResponse.java
@@ -4,18 +4,21 @@ import lombok.Data;
 
 import java.math.BigDecimal;
 
-        @Data // generes como , Getters , toString, equals, hashCode
-        public class AutoResponse {
-
-            private Long identificador;
-            private String marca;
-            private String modelo;
-            private Integer anio;
-            private String color;
-            private Integer kilometraje;
-            private String tipo;
-            private BigDecimal precio;
-            private String descripcion;
-            private String imagenUrl;
-            private String estado;
-        }
+@Data
+public class AutoResponse {
+    private Long id;
+    private String marca;
+    private String modelo;
+    private Integer anio;
+    private String color;
+    private Integer kilometraje;
+    private String tipoCombustible;
+    private String transmision;
+    private Integer cilindrada;
+    private Integer potencia;
+    private Integer stock;
+    private BigDecimal precio;
+    private String descripcion;
+    private String imagenUrl;
+    private String estado;
+}
