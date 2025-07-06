6ed8445caab27ce6eb2e67287b3154eee3a99a89
roosivelt-td <roosivelt.td@gmail.com>
2025-07-06 15:57:49 -0500

    actualizacion baquent

--- src/main/java/automoviles/repository/ClienteRepository.java
+++ src/main/java/automoviles/repository/ClienteRepository.java
@@ -4,8 +4,11 @@ import automoviles.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
+    Collection<Cliente> findByDni(Integer dni);
 
 }
